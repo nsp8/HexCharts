857bbe7f49672cb7cb441911dbf19b60ef19f33b
nsp8 <nishantparmar248@gmail.com>
2020-07-08 22:14:27 +0530

    Adding dynamic HexGrid on about.html

--- hive/hex_grid/urls.py
+++ hive/hex_grid/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='hex-home'),
+    path('about/', views.about, name='hex-about'),
 ]
--- hive/hex_grid/views.py
+++ hive/hex_grid/views.py
@@ -8,3 +8,12 @@ def home(request):
         'countries': utils.get_hex_data()
     }
     return render(request, 'hex_grid/home.html', context)
+
+
+def about(request):
+    context = {
+        'hexes_in_even': range(5),
+        'hexes_in_odd': range(4),
+        'total_columns': range(1, 16)
+    }
+    return render(request, 'hex_grid/about.html', context)
